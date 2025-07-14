46b6367191187ad3c403f48a5047cf9d4dbfe3e3
Derek Shaw <deshaw@microsoft.com>
2025-07-14 10:53:35 -0700

    config fixup

--- classifier/utils/config.py
+++ classifier/utils/config.py
@@ -27,35 +27,9 @@ LABEL_LIST = [
 
 # Text transformation patterns
 # This is used to simplify the desciption field to make it easier for the model to interpret.
-# TODO: Move this to a db, and load from there? Or perhaps another CSV file.
+# Add transactions lables here in the form of "pattern": "label" where the pattern is the label
+# for a transaction and the lable is a simplified form. 
 TEXT_TRANSFORMS = {
-    "FIDELITY": "fidelity",
-    "BK OF AMER VISA": "bank of america visa payment",
-    "WA ST EMPLOY SEC  - UI BENEFIT": "unemployment benefit",
-    "CITY OF SNOQUALMIE 38624 SE RIVER ST": "sewer and water utilities",
-    "CITYOFSNOQUALMIE": "sewer and water utilities",
-    "Withdrawal Transfer to 9345974472": "audi car payment",
-    "Debit JPMorgan Chase": "subaru car payment",
-    "VZWRLSS": "verizon wireless",
-    "APPLE CASH SENT MONEY 1 INFINITE LOOP": "apple cash transfer",
-    "CHEVRON": "fuel",
-    "CHIPOTLE": "chipotle",
-    "FARMERS INS": "farmers insurance",
-    "NEWREZ-SHELLPOIN": "shellpoint mortgage servicing",
-    "AMEX EPAYMENT": "american express payment",
-    "NETFLIX": "netflix",
-    "HAIRY HOUDINI": "dog poop pickup",
-    "ZOOM.COM": "zoom subscription",
-    "LOWE'S": "lowes",
-    "PUGET SOUND ENER": "puget sound energy",
-    "SAFEWAY FUEL": "fuel",
-    "COMCAST CABLE": "comcast cable",
-    "UPSTART NETWORK": "upstart loan payment",
-    "AMAZON.COM": "amazon goods",
-    "Infiniti  - Auto Lease": "infiniti lease",
-    "MACYS  - ONLINE PMT": "macys credit card payment",
-    "WASTE MANAGEMENT": "waste management",
-    "Debit KEYBANKNA": "key bank line of credit payment",
-    "******3532": "first tech primary mortgage",
-
+"NETFLIX": "netflix",
+"AMAZON.COM": "amazon goods"
 }
